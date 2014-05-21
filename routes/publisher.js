5ae7d05b6cf76217e09a18d5de9cc57252ad2cf5
Naouak <tardiel@gmail.com>
2014-05-21 22:43:24 +0200

    Adding a page display for a publisher.

--- routes/publisher.js
+++ routes/publisher.js
@@ -2,6 +2,9 @@ var Promise = require("es6-promise").Promise;
 var database = require("./../database");
 var Publisher = require("./../models/publisher").Publisher;
 
+var template = require("./../template");
+var Page = require("./../page").Page;
+
 module.exports = function(router){
     router.get("/publishers/", function(req, res){
         database.find("publishers").then(function(publishers){
@@ -10,14 +13,14 @@ module.exports = function(router){
     });
 
     router.get("/publisher/isbn/:isbn", function(req,res){
-        console.log(req.params.isbn);
-         Publisher.getPublisherByISBNKey(req.params.isbn).then(function(publisher){
-                publisher.getData().then(function(data){
-                    res.send(data);
-                });
-         }, function(err){
-             console.log(err);
-             res.send(err);
-         });
+        var tpl = template.loadTemplate("publisher");
+        var page = new Page("basic");
+        Publisher.getPublisherByISBNKey(req.params.isbn).then(function(publisher){
+            return publisher.getData()
+        }).then(function(data){
+            return tpl.then(function(tmpl){return tmpl(data);});
+        }).then(function(content){
+            page.setContent("body",content).render(res);
+        });
     });
 };
